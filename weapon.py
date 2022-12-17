3393847455beb8dec40845411322a8fc833ec252
Rolandevitt <rolandevitt@gmail.com>
2022-12-18 00:31:42 +0300

    wip

--- weapon.py
+++ weapon.py
@@ -1,29 +1,42 @@
-from dice import Dice
+import json
 
 
 class Weapon:
 
-    def __init__(self, dice, num_rolls, name, rarity, cost):
+    def __init__(self, dice, num_rolls, name, cost):
         self.dice = dice
         self.num_rolls = num_rolls
         self.name = name
-        self.rarity = rarity
         self.cost = cost
 
     def damage(self):
         return self.dice.roll(self.num_rolls)
 
     def __repr__(self):
-        return f"\n{self.name}, damage: {self.num_rolls}d{self.dice}, rarity: {self.rarity}, cost: {self.cost}"
+        return f"\n{self.name}, damage: {self.num_rolls}d{self.dice}, cost: {self.cost}"
 
 
-weapons = [
-    Weapon(Dice(6), 1, "Long sword", 0.5, 100),
-    Weapon(Dice(6), 2, "Wolf Knight's Greatsword", 0.8, 800),
-    Weapon(Dice(12), 1, "Uchigatana", 0.6, 500),
-    Weapon(Dice(4), 2, "Sellsword Twinblades", 0.5, 200),
-    Weapon(Dice(4), 1, "Dagger", 0.4, 50)
-]
+# weapons = [
+#     Weapon(6, 1, "Long sword", 100),
+#     Weapon(6, 2, "Wolf Knight's Greatsword", 800),
+#     Weapon(12, 1, "Uchigatana", 500),
+#     Weapon(4, 2, "Sellsword Twinblades", 200),
+#     Weapon(4, 1, "Dagger", 50)
+# ]
+weapons_json = """
+{"weapons": [
+    {"dice": 6, "num_rolls": 1, "name": "Long sword", "cost": 100},
+    {"dice": 6, "num_rolls": 2, "name": "Wolf Knight's Greatsword", "cost": 800},
+    {"dice": 12, "num_rolls": 1, "name": "Uchigatana", "cost": 500},
+    {"dice": 4, "num_rolls": 2, "name": "Sellsword Twinblades", "cost": 200},
+    {"dice": 4, "num_rolls": 1, "name": "Dagger", "cost": 50}
+]}
+"""
+
+weapons = json.loads(weapons_json)
+
+with open('config.json', 'w') as file:
+    json.dump(weapons_json, file, indent=2)
 
 
 class Armor:
@@ -46,4 +59,4 @@ armors = [
 
 
 if __name__ == "__main__":
-    print(weapons[0].damage())
+    print(weapons)
